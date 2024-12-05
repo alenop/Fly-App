fb665b5eff6e9eea80b7584f3aae607ca6b9531f
Pierre Sorli <ps@ikno.fr>
2024-12-05 16:38:29 +0100

    fix

--- Back/dataService/fakeBdd.ts
+++ Back/dataService/fakeBdd.ts
@@ -1,23 +1,42 @@
 import { User } from "../model/User"
-
+import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcrypt';
 export class FakeBdd {
 
 private database:{[key:string]:User} = {
 
 }
 
-    create(user:User,password:string){
-        this.database[password] = user;
+    async create(user:User){
+        const uuid = uuidv4();
+        user.id = uuid;
+        user.password = await this.hashPassword(user.password);
+        this.database[uuid] = user;
         console.log(this.database);
     }
 
-    check(username:string,password:string){
+    async check(username:string,password:string){
         console.log(this.database);
-        return this.database[password].mail === username || this.database[password].username;
+        for (const i of Object.entries(this.database)){
+            if (i[1].username === username && await this.verifyPassword(password,i[1].password)){
+                return i[1];
+            }
+        }
+        return false;
     }
 
-    get(password:string){
-        return this.database[password];
+    get(uuid:string){
+        return this.database[uuid];
     }
+
+    async hashPassword(password: string): Promise<string> {
+        const saltRounds = 10; // Adjust this for security/performance tradeoff
+        return await bcrypt.hash(password, saltRounds);
+      };
+      
+      // Compare a password with a hash
+      async verifyPassword (password: string, hashedPassword: string): Promise<boolean> {
+        return await bcrypt.compare(password, hashedPassword);
+      };
 
 }
--- Back/model/User.ts
+++ Back/model/User.ts
@@ -2,10 +2,12 @@ export class User {
     username:string
     mail:string;
     id?:string;
+    password:string;
 
 
-    constructor(username:string,mail:string){
+    constructor(username:string,mail:string,password:string){
         this.username=username;
         this.mail=mail;
+        this.password=password;
     }
 }
