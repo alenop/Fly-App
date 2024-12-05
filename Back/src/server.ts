0a6aae68ebd6c7ed7ad91de41bdd804a4dd54f67
Pierre Sorli <ps@ikno.fr>
2024-12-05 16:03:55 +0100

    fix

--- Back/src/server.ts
+++ Back/src/server.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
-import  routerUser  from './routes/userRoutes';
-// Load environment variables from .env file
+import routerUser from './routes/userRoutes'; // Assuming you have userRoutes
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
@@ -9,7 +9,15 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
-app.use('/user',routerUser);
+
+
+
+// Apply CORS middleware globally
+app.use(cors());
+
+// Use the user routes
+app.use('/user', routerUser);
+
 // Define a simple route
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, Express with TypeScript!');
