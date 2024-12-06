c794c49324efdf10be95d7b1d0825826d1091c09
StevenRaoul <sraoul@et.intechinfo.fr>
2024-12-06 11:04:07 +0100

    ajout de liens dans le bandeau de menu + correction du css du bandeau

--- Front/fly-app/src/router/index.ts
+++ Front/fly-app/src/router/index.ts
@@ -6,11 +6,11 @@ import SearchView from '@/views/SearchView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomeView,
+    // },
     {
       path: '/about',
       name: 'about',
@@ -25,7 +25,7 @@ const router = createRouter({
       component: RegisterView,
     },
     {
-      path: '/search',
+      path: '/',
       name: 'search',
       component: SearchView,
     },
