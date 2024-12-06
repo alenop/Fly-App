2a430a4c6d7a77a5160bf827b9024c00d120fe2c
Pierre Sorli <ps@ikno.fr>
2024-12-06 11:58:31 +0100

    fix and add method to add everything to the fake BDD

--- Back/dataService/fakeBdd.ts
+++ Back/dataService/fakeBdd.ts
@@ -1,45 +1,36 @@
 import { User } from "../models/User"
-import { v4 as uuidv4 } from 'uuid';
-import bcrypt from 'bcrypt';
-export class FakeBdd {
 
-private database:{[key:string]:User} = {
+import {BDD} from "./Interface/BDD";
+export class FakeBdd implements BDD{
+
+private database:{[key:string]:any} = {
 
 }
-
-    async create(user:User){
-        if(!await this.check(user.username,user.password)){
-            return false;
+    async createObject(obj:any,uuid:string,type:string){
+        if(this.database.type){
+            this.database.type[uuid]=obj;
+        }else {
+            this.database.type={};
+            this.database.type[uuid]=obj;
         }
-        const uuid = uuidv4();
-        user.id = uuid;
-        user.password = await this.hashPassword(user.password);
-        this.database[uuid] = user;
-        console.log(this.database);
     }
 
-    async check(username:string,password:string){
-        console.log(this.database);
-        for (const i of Object.entries(this.database)){
-            if (i[1].username === username && await this.verifyPassword(password,i[1].password)){
-                return i[1];
-            }
-        }
-        return false;
+    getAllObject(type:string){
+        return this.database[type];
     }
 
-    get(uuid:string){
+    async createUser(user:User,uuid:string){
+        this.database[uuid]=user;
+    }
+    getAll(){
+        return this.database;
+    }
+    getByUuid(uuid:string){
         return this.database[uuid];
     }
 
-    async hashPassword(password: string): Promise<string> {
-        const saltRounds = 10; // Adjust this for security/performance tradeoff
-        return await bcrypt.hash(password, saltRounds);
-      };
-      
-      // Compare a password with a hash
-      async verifyPassword (password: string, hashedPassword: string): Promise<boolean> {
-        return await bcrypt.compare(password, hashedPassword);
-      };
+    delete(uuid:string){
+
+    }
 
 }
--- Back/src/routes/userRoutes.ts
+++ Back/src/routes/userRoutes.ts
@@ -1,8 +1,10 @@
 import { Router, Request, Response } from 'express';
 import {FakeBdd} from '../../dataService/fakeBdd';
 import { User } from '../../models/User';
+import {UserController} from '../../controllers/userController';
 const router = Router();
 const database = new FakeBdd();
+const controller = new UserController();
 router.get('/', (req: Request, res: Response) => {
   res.json({ message: 'List of users' });
 });
@@ -14,13 +16,12 @@ router.post('/users', (req: Request, res: Response) => {
 
 router.post('/login',(req: Request, res: Response) => {
     const { username, password } = req.body;
-    res.json({message:database.check(username,password)});
+    res.json({message:controller.check(username,password)});
 })
 
 router.post('/sign',(req: Request, res: Response) => {
     const { username,mail, password } = req.body;
-    console.log(req.body);
-    if(!database.create(new User(username,mail,password))){
+    if(!controller.createUser(new User(username,mail,password))){
         res.json({message:"all is not ok account already create before"});
     }
     res.json({message:"all is ok"});
