644a04f03d519c1a76c2a6f464727f531b8016d1
StevenRaoul <sraoul@et.intechinfo.fr>
2024-12-09 14:17:40 +0100

    Ajout de la monnaie selectionable parmi une liste brute + Les prix affichés sont modifiés en fonction de la monnaie choisie

--- Front/fly-app/src/views/SearchView.vue
+++ Front/fly-app/src/views/SearchView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="page flex column">
     <Menu />
-    <div class="flex column center search">                 <!-- search bar-->       
+    <div class="flex column center search">               <!-- search bar-->       
       <h1>Accueil</h1>
       <div class="flex row search-item">
         <div class="flew column">
@@ -22,6 +22,14 @@
         </div>
         <button>Recherche</button>
       </div>
+      <div class="flex column">
+        <p>Monnaie: </p>
+        <select v-model="reservecurrency" id="currency">
+          <option v-for="currency in currencies" :value="currency"> 
+            {{ currency.code }}
+          </option>
+        </select>
+      </div>
     </div>
 
     <div class="flex column left flights">                 <!-- flight list-->
@@ -32,12 +40,12 @@
           <h2>Arrivée : {{ vol.cityArrival }}, {{ vol.countryArrival }} ({{ vol.codeArrival }})</h2>
           <h4>Durée : {{ vol.duration }}h</h4>
           <h4>Places disponibles : {{ vol.seatavailable }}/{{ vol.seat }}</h4>
-          <h4>Prix : {{ vol.price }}€</h4>
+          <h4>Prix : {{ calculatePriceCurrency(vol) }}{{ reservecurrency.symbol }}</h4>
         </div>
         <div>
           <form @submit.prevent="reserve(vol)" class="flex column flightinput">
-            <div class="flex row">
-              <p>Nombre de bagages (100€/bagages) : </p>
+            <div class="flex row minispace">
+              <p>Nombre de bagages ({{ calculateLuggageCurrency() }}{{ reservecurrency.symbol }}/bagages) : </p>
               <select v-model="reserveluggage">
                 <option>0</option>
                 <option>1</option>
@@ -52,12 +60,14 @@
                 <option>10</option>
               </select>
             </div>
-            <div class="flex row">
+            <div class="flex row minispace">
               <p>Date :</p>
               <input type="date" id="date" v-model="reservedate">
             </div>
-            <h4>Prix total : {{ calculerPrixTotal(vol) }}€</h4>
-            <button class="reservationbutton">Réserver une place</button>
+            <div class="flex row minispace">
+              <h4>Prix total : {{ calculatePriceTotal(vol) }}{{ reservecurrency.symbol }}</h4>
+              <button class="reservationbutton">Réserver une place</button>
+            </div>
         </form>
         </div>
       </div>
@@ -95,7 +105,7 @@
 .search {
   margin-top: 5vh; 
   margin-bottom: 10vh;
-
+  gap: 5vh;
 }
 
 .search-item {
@@ -120,6 +130,10 @@
 
  .flightinput {
   row-gap: 5vh;
+ }
+
+ .minispace {
+  gap: 2vw;
  }
 
  h1 {
@@ -152,6 +166,38 @@ import Menu from '../components/Menu.vue'
 export default {
   data() {
     return {
+      currencies: [
+        {
+          code: 'EUR',
+          name: 'Euros',
+          rate: '1',
+          symbol: '€',
+        },
+        {
+          code: 'USD',
+          name: 'Dollars Américains',
+          rate: '1.0581',
+          symbol: '$',
+        },
+        {
+          code: 'JPY',
+          name: 'Yen',
+          rate: '159.25',
+          symbol: '¥'
+        },
+        {
+          code: 'GPB',
+          name: 'Livres Sterling',
+          rate: '0.82855',
+          symbol: '£'
+        },
+        {
+          code: 'CAD',
+          name: 'Dollars Canadien',
+          rate: '1.4882',
+          symbol: '$'
+        },
+      ],
       vols: [
         {
           id: '12345',
@@ -195,6 +241,7 @@ export default {
       ],
       reserveluggage: '0',
       reservedate: new Date().toISOString().substr(0, 10),
+      reservecurrency: { code: 'EUR', name: 'Euros', rate: '1', symbol: '€'},
     }
   },
   components: {
@@ -202,13 +249,20 @@ export default {
   },
   methods: {
     reserve(vol) {
-      const totalprice = this.calculerPrixTotal(vol);
-      alert("Merci pour votre reservation ! \nNombres de bagages : " + this.reserveluggage + "\nDate de réservation : " + this.reservedate + "\nPrix total : " + totalprice + "€");
+      const totalprice = this.calculatePriceTotal(vol);
+      alert("Merci pour votre reservation pour le vol Départ : " + vol.cityDeparture + " (" + vol.codeDeparture + ") - " + vol.cityArrival + " (" + vol.codeArrival + ")" + " ! \nNombres de bagages : " + this.reserveluggage + "\nDate de réservation : " + this.reservedate + "\nPrix total : " + totalprice + this.reservecurrency.symbol);
     },
-    calculerPrixTotal(vol) {
-      const luggageprice = parseInt(this.reserveluggage) * 100;
-      return parseInt(vol.price) + luggageprice;
+    calculatePriceTotal(vol) {
+      const luggageprice = parseInt(this.reserveluggage) * this.calculateLuggageCurrency();
+      return this.calculatePriceCurrency(vol) + luggageprice;
     },
+    calculateLuggageCurrency() {
+      return parseInt(parseFloat(this.reservecurrency.rate) * 100);
+    },
+    calculatePriceCurrency(vol) {
+      const rate = parseFloat(this.reservecurrency.rate);
+      return parseInt(parseInt(vol.price) * rate);
+    }
   },
 };
 </script>
