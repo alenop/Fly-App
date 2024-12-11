dc912e051a56f77dc65c5b2a89ed13a2071ea446
jacopo <bonardi14@gmail.com>
2024-12-11 09:37:42 +0100

    add controller and routes for flights

---
+++ Back/src/routes/flightRoutes.ts
@@ -0,0 +1,10 @@
+import { Router, Request, Response } from 'express';
+import { MainController } from '../../controllers/mainController';
+import { Flight } from '../../models/Flight';
+import { FlightController } from '../../controllers/flightController'
+const router = Router();
+const flightController = new FlightController;
+
+router.get('/users', (req: Request, res: Response) => {
+    res.json({ message: 'Users yeah', users:flightController.getAllFlights() });
+  });
