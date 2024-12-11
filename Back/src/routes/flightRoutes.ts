7d61a4cc842bfe11ba60d4dc45f478effd6a8a3c
jacopo <bonardi14@gmail.com>
2024-12-11 09:45:00 +0100

    fix route name

--- Back/src/routes/flightRoutes.ts
+++ Back/src/routes/flightRoutes.ts
@@ -5,6 +5,6 @@ import { FlightController } from '../../controllers/flightController'
 const router = Router();
 const flightController = new FlightController;
 
-router.get('/users', (req: Request, res: Response) => {
+router.get('/flights', (req: Request, res: Response) => {
     res.json({ message: 'Users yeah', users:flightController.getAllFlights() });
   });
