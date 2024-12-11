0b715fd6d84b6f3e7b677efc44d841e4c85de0a3
jacopo <bonardi14@gmail.com>
2024-12-11 11:54:23 +0100

    fix

--- Back/controllers/airportController.ts
+++ Back/controllers/airportController.ts
@@ -2,7 +2,7 @@ import { AirportBdd } from '../dataService/BDD/AirportBdd'
 
 export class AirportController{
     private bdd = new AirportBdd;
-    async getAirportById(airport: string){
+    async getAirportById(airport: number){
         return this.bdd.getAirportById;
     }
     async getAllAirports(){
