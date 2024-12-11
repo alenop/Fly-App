1bcaf9bdaa9035cabba62540d83ae71618b63ef9
StevenRaoul <sraoul@et.intechinfo.fr>
2024-12-11 14:01:06 +0100

    ajout de getairports début

--- Back/controllers/airportController.ts
+++ Back/controllers/airportController.ts
@@ -3,7 +3,7 @@ import { AirportBdd } from '../dataService/BDD/AirportBdd'
 export class AirportController{
     private bdd = new AirportBdd;
     async getAirportById(airport: number){
-        return this.bdd.getAirportById;
+        return this.bdd.getAirportById(airport);
     }
     async getAllAirports(){
         return this.bdd.getAllAirports;
